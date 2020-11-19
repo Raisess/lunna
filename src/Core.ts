ba4f85318c74cae557b4863949a5fd7f64913b64
Raisess <raisechif@gmail.com>
2020-11-19 12:00:58 -0300

    created onMessage and onMessageExec methods

--- src/Core.ts
+++ src/Core.ts
@@ -6,6 +6,8 @@ export default class Core {
 	private token:  string;
 	// commands storage
 	private commands: Array<[string, Function]> = [];
+	// special messages storage
+	private messages: Array<[string, Function]> = [];
 
 	constructor(prefix: string = "!", token: string = "") {
 		this.prefix = prefix;
@@ -21,7 +23,11 @@ export default class Core {
 			if (message.author.bot) return;
 
 			// check if is a command
-			if (message.content.startsWith(this.prefix)) this.onCommandExec(message);
+			if (message.content.startsWith(this.prefix)){ 
+				this.onCommandExec(message);
+			} else {
+				this.onMessageExec(message);
+			}
 		});
 
 		this.client.login(this.token);
@@ -31,9 +37,9 @@ export default class Core {
 	private onCommandExec(message: any): void {
 		const commandOnly: string = message.content.split(" ")[1].toLowerCase();
 
-		for (const command of this.commands) {
-			if (commandOnly === command[0]) {
-				command[1](message, this.client);
+		for (const commandTuple of this.commands) {
+			if (commandOnly === commandTuple[0]) {
+				commandTuple[1](message, this.client);
 				return;
 			}
 		}
@@ -44,6 +50,25 @@ export default class Core {
 	// store command
 	public onCommand(command: string, callback: Function): void {
 		this.commands.push([command.toLowerCase(), callback]);
+	}
+
+	// execute message callback
+	private onMessageExec(message: any): void {
+		const content: string = message.content.toLowerCase();
+
+		for (const messageTuple of this.messages) {
+			if (content === messageTuple[0]) {
+				messageTuple[1](message, this.client);
+				return;
+			}
+		}
+
+		return;
+	}
+
+	// store a special message
+	public onMessage(content: string, callback: Function): void {
+		this.messages.push([content.toLowerCase(), callback]);
 	}
 }
 
