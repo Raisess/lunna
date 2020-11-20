f6a4bdcfa11441fc6d37bb07ac4f86780883ec95
Raisess <raisechif@gmail.com>
2020-11-20 10:56:40 -0300

    created exec '$' command

---
+++ src/commands/execCommand.ts
@@ -0,0 +1,26 @@
+import { ICommand, ICommandMessage } from "../interfaces/ICommand";
+
+import adminCommandOptions from "../adminCommandsOptions.json";
+
+import { exec } from "child_process";
+
+export const execCommand: ICommand = {
+	name:     "$",
+	callback: execCommandCallback,
+	options:  adminCommandOptions["eval"]
+};
+
+function execCommandCallback(commandMessage: ICommandMessage): void {
+	const toExec: string = commandMessage.args ? commandMessage.args.join(" ") : "";
+
+	exec(toExec, (err: any, stdout: string, stderr: string) => {
+		if (err) {
+			commandMessage.message.react("⚠️");
+			return;
+		}
+
+		if (stdout) commandMessage.message.channel.send(stdout, { code: "shell" });
+		if (stderr) commandMessage.message.channel.send(stderr, { code: "shell" });
+	});
+}
+
--- src/wrappers/commandsWrapper.ts
+++ src/wrappers/commandsWrapper.ts
@@ -1,12 +1,16 @@
 import { ICommand } from "../interfaces/ICommand";
 
+// admin
 import { evalCommand } from "../commands/evalCommand";
 import { debugArgsCommand } from "../commands/debugArgsCommand";
+import { execCommand } from "../commands/execCommand";
+// all
 import { pingCommand } from "../commands/pingCommand";
 
 export const commands: Array<ICommand> = [
 	evalCommand,
 	debugArgsCommand,
+	execCommand,
 	pingCommand
 ];
 
