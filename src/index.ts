84b051ae9d9d892b4962727f9d984669d7aa233a
Raisess <raisechif@gmail.com>
2020-11-20 09:38:14 -0300

    create special words wrapper && create some special words in json data folder

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import "dotenv/config";
 
 import BotCore from "./BotCore";
 import { commands } from "./commandsWrapper";
+import { specialWords } from "./specialWordsWrapper";
 
 const bot: BotCore = new BotCore(process.env.BOT_PREFIX, process.env.BOT_TOKEN);
 
 for (const command of commands) {
 	bot.onCommand(command.name, command.callback, command.options);
+}
+
+for (const specialWord of specialWords) {
+	bot.onMessageIncludes(specialWord.words, specialWord.callback);
 }
 
---
+++ src/specialWordsWrapper.ts
@@ -0,0 +1,14 @@
+import { ISpecialWord } from "./interfaces/ISpecialWord";
+
+import specialWordsJson from "./jsonData/specialWords.json";
+
+export const specialWords: Array<ISpecialWord> = specialWordsJson.map((json: any) => {
+	return {
+		words: json.words,
+		callback: (message: any) => {
+			if (json.reactWith) message.react(json.reactWith);
+			if (json.respondWith) message.channel.send(json.respondWith);
+		}
+	}
+});
+
