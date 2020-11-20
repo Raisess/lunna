123b712307bdd8fafd536dbba85032df610e8490
Raisess <raisechif@gmail.com>
2020-11-20 15:14:06 -0300

    convert channel name to lowercase

--- src/Core/validators/channelValidator.ts
+++ src/Core/validators/channelValidator.ts
@@ -1,6 +1,6 @@
 export default function channelValidate(channel: string, channelArray: Array<string>): boolean {
 	if (channelArray && channelArray.length > 0) {
-		return channelArray.includes(channel);
+		return channelArray.includes(channel.toLowerCase());
 	}
 
 	return false;
