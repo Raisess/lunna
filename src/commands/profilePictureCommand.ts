5a57eadbe535a96b7a116f46c5464e46527552bc
Raisess <raisechif@gmail.com>
2020-11-21 08:56:07 -0300

    updated pfp command, adding more quality to image and adding the image link

--- src/commands/profilePictureCommand.ts
+++ src/commands/profilePictureCommand.ts
@@ -8,13 +8,15 @@ export const profilPictureCommand: ICommand = {
 };
 
 function profilPictureCommandCallback(commandMessage: ICommandMessage): void {
-	const user: any = commandMessage.message.mentions.users.first() || commandMessage.message.author;
-	
+	const user:   any    = commandMessage.message.mentions.users.first() || commandMessage.message.author;
+	const avatar: string = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
+
 	const embed: MessageEmbed = new MessageEmbed();
 
 	embed.setColor("RANDOM");
 	embed.setAuthor(`${user.username} profile picture:`);
-	embed.setImage(user.avatarURL());
+	embed.setImage(avatar);
+	embed.setDescription(`[Image Link](${avatar})`);
 
 	commandMessage.message.channel.send(embed);
 }
