7a37710609b01b933dfc5074887efce8b1d2ca0f
Raisess <raisechif@gmail.com>
2020-11-23 10:42:22 -0300

    set ts version to es2017 && created server to deploy on heroku

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import BotCore from "./Core/BotCore";
 import { commands } from "./wrappers/commandsWrapper";
 import { specialWords } from "./wrappers/specialWordsWrapper";
 
+import http from "http";
+import SmolHttp from "./libraries/smol-http/src/SmolHttp";
+
+// bot
 const bot: BotCore = new BotCore(new Client(), process.env.BOT_PREFIX, process.env.BOT_TOKEN);
 
 for (const command of commands) {
@@ -15,4 +19,14 @@ for (const command of commands) {
 for (const specialWord of specialWords) {
 	bot.onMessageIncludes(specialWord.words, specialWord.callback);
 }
+
+// server
+const server: SmolHttp = new SmolHttp(parseInt(process.env.PORT || "1939"));
+
+server.get("/", () => "");
+
+// keep server alive
+setInterval(() => {
+	http.get(process.env.SERVER_URL ? `https://${process.env.SERVER_URL}` : "http://localhost:1939");
+}, 5 * 60000);
 
