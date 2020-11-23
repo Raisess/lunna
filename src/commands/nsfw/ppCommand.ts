475fd7b9407313b47cd496d3594c25f9a441aa04
Raisess <raisechif@gmail.com>
2020-11-23 09:43:26 -0300

    if try to get lunna pp info always return 30cm yaya

--- src/commands/nsfw/ppCommand.ts
+++ src/commands/nsfw/ppCommand.ts
@@ -14,12 +14,13 @@ interface IPP {
 	draw: (ppSize: number) => string;
 }
 
-function ppCommandCallback(commandMessage: ICommandMessage): void {
-	const user:   any    = commandMessage.message.mentions.users.first() || commandMessage.message.author;
-	const avatar: string = getUserAvatar(user);
+function ppCommandCallback(commandMessage: ICommandMessage, client: any): void {
+	const user:    any     = commandMessage.message.mentions.users.first() || commandMessage.message.author;
+	const avatar:  string  = getUserAvatar(user);
+	const isLunna: boolean = user.id === client.user.id;
 
 	const pp: IPP = {
-		size: Math.round(Math.random() * 30),
+		size: !isLunna ? Math.round(Math.random() * 30) : 30,
 		draw: (ppSize: number): string => {
 			let draw: string = "";
 
@@ -38,7 +39,7 @@ function ppCommandCallback(commandMessage: ICommandMessage): void {
 	embed.setTitle(`${user.username}'s pp info:`);
 	embed.addField("PP size:", `${pp.size}cm`);
 	embed.addField("PP representation:", `8${pp.draw(pp.size)}D`);
-	embed.setFooter(`${pp.size > 10 ? "U have a big pp bro." : "Oh dear smol pp, but don't worry its kinda cute."}`);
+	embed.setFooter(`${pp.size > 10 ? (isLunna ? "I have the biggest pp in the world bud haha." : "U have a big pp bro.") : "Oh dear smol pp, but don't worry its kinda cute."}`);
 
 	commandMessage.message.channel.send(embed);
 }
