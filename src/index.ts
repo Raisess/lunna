90090c84ea1932c710581e2b1eb3822792cf49e8
Raisess <raisechif@gmail.com>
2020-11-24 08:23:05 -0300

    added an env to enable server maintenance

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,24 @@ import BotCore from "./Core/BotCore";
 import { commands } from "./wrappers/commandsWrapper";
 import { specialWords } from "./wrappers/specialWordsWrapper";
 
-const bot: BotCore = new BotCore(new Client(), process.env.BOT_PREFIX, process.env.BOT_TOKEN);
+if (process.env.MAINTENANCE === "false") {
+	const bot: BotCore = new BotCore(new Client(), process.env.BOT_PREFIX, process.env.BOT_TOKEN);
 
-for (const command of commands) {
-	bot.onCommand(command.name, command.description, command.callback, command.options);
-}
-
-for (const specialWord of specialWords) {
-	bot.onMessageIncludes(specialWord.words, specialWord.callback);
-}
-
-app.listen(process.env.PORT || 1939);
-
-// keep server alive
-setInterval(() => {
-	if (process.env.SERVER_URL && process.env.SERVER_URL !== "") {
-		https.get(process.env.SERVER_URL);
+	for (const command of commands) {
+		bot.onCommand(command.name, command.description, command.callback, command.options);
 	}
-}, 5 * 60000);
+
+	for (const specialWord of specialWords) {
+		bot.onMessageIncludes(specialWord.words, specialWord.callback);
+	}
+
+	app.listen(process.env.PORT || 1939);
+
+	// keep server alive
+	setInterval(() => {
+		if (process.env.SERVER_URL && process.env.SERVER_URL !== "") {
+			https.get(process.env.SERVER_URL);
+		}
+	}, 5 * 60000);
+}
 
