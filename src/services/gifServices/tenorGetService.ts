45ec1e45e58634d59a1d2367409b2c647259123e
Raisess <raisechif@gmail.com>
2020-11-27 09:48:12 -0300

    fixed tenor and pixabay search regex replace

--- src/services/gifServices/tenorGetService.ts
+++ src/services/gifServices/tenorGetService.ts
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 export default async function tenorGetService(search: string): Promise<string> {
-	const req: any = await fetch(`https://api.tenor.com/v1/search?q=${search}&lmt=10&key=${process.env.TENOR_API_KEY}`);
+	const req: any = await fetch(`https://api.tenor.com/v1/search?q=${search.replace(/\s+/g, "+")}&lmt=10&key=${process.env.TENOR_API_KEY}`);
 
 	const gifs:      any = await req.json();
 	const randomGif: any = await gifs.results[Math.round(Math.random() * (gifs.results.length - 1))];
--- src/services/imageServices/pixabayGetService.ts
+++ src/services/imageServices/pixabayGetService.ts
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 export default async function pixabayGetService(search: string): Promise<string | undefined> {
 	const apiKey: string = process.env.PIXABAY_API_KEY ? process.env.PIXABAY_API_KEY : "";
 
-	const request:  any = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${search.replace(/\+s/g, "+")}`);
+	const request:  any = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${search.replace(/\s+/g, "+")}`);
 	const response: any = await request.json();
 
 	const allImages: Array<any> = await response.hits;
