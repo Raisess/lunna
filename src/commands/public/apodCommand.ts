6eb83d27094949ef8cd636f30688af1467fd737f
Raisess <raisechif@gmail.com>
2020-12-02 15:06:17 -0300

    added a error catch

--- src/commands/public/apodCommand.ts
+++ src/commands/public/apodCommand.ts
@@ -11,20 +11,26 @@ export const apodCommand: ICommand = {
 };
 
 async function apodCommandCallback(commandMessage: ICommandMessage): Promise<void> {
-	const args: string = commandMessage.args?.join(" ") || "";
+	try { 
+		const args: string = commandMessage.args?.join(" ") || "";
 
-	const apod: any = await apodNasaGetService(args);
+		const apod: any = await apodNasaGetService(args);
 
-	const embed: MessageEmbed = new MessageEmbed();
+		const embed: MessageEmbed = new MessageEmbed();
 
-	embed.setColor("RANDOM");
-	embed.setTitle(apod.title);
-	embed.addField("Date:", apod.date);
-	embed.setImage(apod.hdurl);
-	embed.setDescription(apod.explanation);
-	embed.setFooter("Powered by NASA");
-	embed.setTimestamp();
+		embed.setColor("RANDOM");
+		embed.setTitle(apod.title);
+		embed.addField("Date:", apod.date);
+		embed.setImage(apod.hdurl);
+		embed.setDescription(apod.explanation);
+		embed.setFooter("Powered by NASA");
+		embed.setTimestamp();
 
-	commandMessage.message.channel.send(embed);
+		commandMessage.message.channel.send(embed);
+	} catch(err) {
+		console.error(err.message);
+
+		commandMessage.message.react("⚠️");
+	}
 }
 
