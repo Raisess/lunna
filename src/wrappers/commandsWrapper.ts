406fe3cabf4d5b22f33fc548b24ae6fae2c72495
Raisess <raisechif@gmail.com>
2020-12-03 11:44:25 -0300

    updated wrapper map

--- src/wrappers/commandsWrapper.ts
+++ src/wrappers/commandsWrapper.ts
@@ -2,18 +2,14 @@ import { ICommand } from "../interfaces/ICommand";
 
 import commandPaths from "../jsonData/commandPaths.json";
 
-export let commands: Array<ICommand> = [];
-
-commandPaths.map((path: string) => {
+export const commands: Array<ICommand> = commandPaths.map((path: string): ICommand => {
 	const command: ICommand = require(`../commands/${path}`);
 
-	commands.push({
+	return {
 		name:        Object.values(command)[0].name,
 		description: Object.values(command)[0].description,
 		callback:    Object.values(command)[0].callback,
 		options:     Object.values(command)[0].options
-	});
-
-	return command;
+	};
 });
 
