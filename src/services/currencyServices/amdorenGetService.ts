f56ffc828cf20f76d0ad02672787bfeb4d2923a7
Raisess <raisechif@gmail.com>
2020-12-05 19:32:12 -0300

    created amdoren currency api get service

---
+++ src/services/currencyServices/amdorenGetService.ts
@@ -0,0 +1,11 @@
+import fetch from "node-fetch";
+
+export default async function amdorenGetService(from: string, to: string, amount?: number): Promise<any> {
+	const req: any = await fetch(`https://www.amdoren.com/api/currency.php?api_key=${process.env.AMDOREN_API_KEY}&from=${from}&to=${to}${amount ? `&amount=${amount}` : ""}`);
+	const res: any = await req.json();
+
+	console.log(res);
+
+	return res;
+}
+
