78d422ebebf73cab044317cb0751d741ea8a610a
Raisess <raisechif@gmail.com>
2020-12-06 17:34:49 -0300

    created error logger

---
+++ src/interfaces/IErrorLogger.ts
@@ -0,0 +1,6 @@
+export interface IErrorLoggerParams {
+	code:    number;
+	message: string;
+	reason?: string;
+}
+
---
+++ src/utils/loggers/ErrorLogger.ts
@@ -0,0 +1,30 @@
+import { IErrorLoggerParams } from "../../interfaces/IErrorLogger";
+
+export default class ErrorLogger {
+	private code:     number;
+	private message:  string;
+	private reason?:  string;
+
+	private timestamp: string = new Date().toLocaleString();
+
+	constructor(params: IErrorLoggerParams) {
+		const { code, message, reason } = params;
+
+		this.code    = code;
+		this.message = message;
+		this.reason  = reason;
+
+		this.log();
+	}
+
+	private log(): void {
+		console.error("\n");
+		console.error("Oops... An error ocurred!");
+		console.error("-----> Error:", this.code);
+		console.error("-----> Message:", this.message);
+		this.reason ? console.error("-----> Reason:", this.reason) : null;
+		console.error("-----> Timestamp:", this.timestamp);
+		console.error("\n");
+	}
+}
+
